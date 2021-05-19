190eb0a1cec343a350e673a483bd44ce886f5ea4
Markus Neugebauer <mail@markusneugebauer.de>
2021-05-19 21:16:39 +0200

    prepared endpoint methods and renamed classes

---
+++ src/main/java/backend/blackbeardboard/BoardApplication.java
@@ -0,0 +1,9 @@
+package backend.blackbeardboard;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class BoardApplication extends Application {
+
+}
---
+++ src/main/java/backend/blackbeardboard/BoardResource.java
@@ -0,0 +1,47 @@
+package backend.blackbeardboard;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/board")
+public class BoardResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getBoard() {
+        return "This will get your board ... later";
+    }
+
+    @GET
+    @Path("/boards")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getBoards() {
+        return "This will get your boards as list ... later";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createBoard() {
+        return "This will create your board ... later";
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateBoard() {
+        return "This will update your board ... later";
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deleteBoard() {
+        return "This will delete your board ... later";
+    }
+
+    @DELETE
+    @Path("/boards")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String deleteBoards() {
+        return "This will delete your boards ... later";
+    }
+
+}
