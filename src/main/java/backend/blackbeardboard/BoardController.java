98c77eae31a60e6828c891184238a539a58f92ab
Markus Neugebauer <mail@markusneugebauer.de>
2021-05-20 23:29:11 +0200

    implement create board

--- src/main/java/backend/blackbeardboard/BoardController.java
+++ src/main/java/backend/blackbeardboard/BoardController.java
@@ -2,16 +2,35 @@ package backend.blackbeardboard;
 
 import jakarta.inject.Singleton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Singleton
 public class BoardController {
 
-    private int counter;
+    private Map<String, Board> boards;
 
-    public int getNumber() {
-        return 4;
+    public BoardController() {
+        boards = new HashMap<>();
     }
 
-    public int count() {
-        return ++counter;
+    public Board getBoard(String name) {
+        return boards.get(name);
+    }
+
+    public Board deleteBoard(String name) {
+        return boards.remove(name);
+    }
+
+    public void addBoard(Board board) {
+        boards.put(board.getName(), board);
+    }
+
+    public boolean noBoards() {
+        return boards.isEmpty();
+    }
+
+    public boolean containsBoard(String name) {
+        return boards.containsKey(name);
     }
 }
--- src/main/java/backend/blackbeardboard/BoardResource.java
+++ src/main/java/backend/blackbeardboard/BoardResource.java
@@ -3,6 +3,7 @@ package backend.blackbeardboard;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/board")
 public class BoardResource {
@@ -13,7 +14,7 @@ public class BoardResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getBoard(@QueryParam("name") String name) {
-        return "This will get your board ... later" + boardController.count();
+        return "This will get your board ... later";
     }
 
     @GET
@@ -25,8 +26,22 @@ public class BoardResource {
 
     @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String createBoard(@QueryParam("name") String name, @QueryParam("deprecationTime") String deprecationTime) {
-        return "This will create your board ... later " + name + " " + deprecationTime;
+    public Response createBoard(@QueryParam("name") String name, @DefaultValue("100") @QueryParam("deprecationTime") String deprecationTime) {
+        if (boardController.containsBoard(name)) {
+            return Response.status(Response.Status.CONFLICT).entity("board \"" + name + "\" already exists").build();
+        }
+        if (name == null || name.equals("")) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("specify name of board to create").build();
+        }
+        int deprecationTimeInt;
+        try {
+            deprecationTimeInt = Integer.parseInt(deprecationTime);
+        } catch (NumberFormatException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("specify deprecation time").build();
+        }
+        Board board = new Board(name, deprecationTimeInt);
+        boardController.addBoard(board);
+        return Response.status(Response.Status.CREATED).entity("successfully created board \"" + name + "\"").build();
     }
 
     @PUT
